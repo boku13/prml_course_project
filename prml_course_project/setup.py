7f41b09ed25649db35ce13dff53266bd5eb22e0a
boku13 <b22ee036@iitj.ac.in>
2024-03-06 19:57:07 +0000

    initialized project directory

---
+++ prml_course_project/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Image Retrieval Using Traditional Machine Learning Techniques',
+    author='Shyam, Neermita, Sakshi, Siddhesh, Bhargavi',
+    license='MIT',
+)
